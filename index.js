a2259a139a5bbeb940f2b2d80c83bb9167ef88b9
Mrunal K <mrunalkapadia@hotmail.com>
2019-05-09 19:10:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,11 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase());
 }
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase()) {
+    return "I can\'t hear you!"
+  } else {
+    return "YES INDEED!"
+  }
+}
